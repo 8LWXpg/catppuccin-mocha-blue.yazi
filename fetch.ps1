4d6074be750d12f207941e8571cc14c61d6f2fb4
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2025-07-22 21:25:06 +0800

    init

---
+++ fetch.ps1
@@ -0,0 +1,8 @@
+$links = @(
+	@('https://github.com/catppuccin/yazi/raw/refs/heads/main/themes/mocha/catppuccin-mocha-blue.toml', 'theme.toml'),
+	@('https://github.com/catppuccin/yazi/raw/refs/heads/main/LICENSE', 'LICENSE')
+)
+
+foreach ($link in $links) {
+	Invoke-WebRequest $link[0] -OutFile $link[1]
+}
