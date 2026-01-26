2583d4d06586b888dce16d005391f3c527e78e2e
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2026-01-26 21:37:24 +0800

    fetch tmtheme

--- fetch.ps1
+++ fetch.ps1
@@ -1,6 +1,7 @@
 $links = @(
 	@('https://github.com/catppuccin/yazi/raw/refs/heads/main/themes/mocha/catppuccin-mocha-blue.toml', 'flavor.toml'),
-	@('https://github.com/catppuccin/yazi/raw/refs/heads/main/LICENSE', 'LICENSE')
+	@('https://github.com/catppuccin/yazi/raw/refs/heads/main/LICENSE', 'LICENSE'),
+	@('https://github.com/catppuccin/bat/raw/refs/heads/main/themes/Catppuccin%20Mocha.tmTheme', 'tmtheme.xml')
 )
 
 foreach ($link in $links) {
